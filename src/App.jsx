2ab673322ba6cd52363f4becff9eaa611d1a7b92
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-03-27 16:39:04 +0800

    [Update] Display dialogue one letter at a time with react useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,41 @@ import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import k from './lib/kaplay';
 
-const { scene, loadSprite, add, pos, sprite, go, wait } = k
+const { scene, loadSprite, add, pos, sprite, go, wait, loop } = k
+
+// Dialouge object
+const conversation = [
+  {
+    person: {
+      name: 'Unknown',
+      sprite: 'test'
+    },
+    dialogue: 'Hello there!',
+    bg: 'cave',
+    options: [],
+    // transition: []
+  },
+  {
+    person: {
+      name: '',
+      sprite: ''
+    },
+    dialogue: 'Anyway. Here is a really long sentence that I want to display on the screen. I hope it works.',
+    bg: 'cave',
+    options: [],
+    // transition: []
+  },
+  {
+    person: {
+      name: '',
+      sprite: ''
+    },
+    dialogue: 'Well. I need a much longer one to work with. Just enough to test the scrollbars. placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...placeholder...',
+    bg: 'cave',
+    options: [],
+    // transition: []
+  }
+]
 
 const initScene = () => {
   scene('game', () => {
@@ -11,11 +45,11 @@ const initScene = () => {
     loadSprite('cave', 'bg/cave.png')
 
     // Create sprites
-    const cave = add([sprite('cave'), pos(0, 0)])
-    const person = add([sprite('test'), pos(0, 0)])
+    // const cave = add([sprite('cave'), pos(0, 0)])
+    // const person = add([sprite('test'), pos(0, 0)])
 
-    console.log(cave)
-    console.log(person)
+    // console.log(cave)
+    // console.log(person)
   })
 
   go('game')
@@ -29,6 +63,16 @@ function App() {
   // const ui = document.querySelector(".ui");
 
   const [isVisible, setIsVisible] = useState(false)
+
+  // Dialouge state
+  const [name, setName] = useState('')
+  const [dialogue, setDialogue] = useState('')
+  const [speed, setSpeed] = useState(0.1)
+  const [index, setIndex] = useState(0)
+
+  // Sprite state
+  const [bg, setBg] = useState({})
+  const [people] = useState([{}, {}, {}])
 
   const uiRef = useRef(null);
 
@@ -47,19 +91,68 @@ function App() {
     }
   }
 
+  const displayDialogue = (index) => {
+    if(conversation[index]){
+      // Create sprites
+      if(!bg.sprite) setBg(add([sprite(conversation[index].bg), pos(0, 0)]))
+
+      if(bg.sprite !== conversation[index].bg){
+        bg.sprite = conversation[index].bg
+      }
+
+      const peopleIndex = conversation[index].person.order? conversation[index].person.order - 1 : 0
+
+      if(!Object.entries(people[peopleIndex]).length){
+        people[peopleIndex] = add([sprite(conversation[index].person.sprite), pos(0, 0)])
+      } else {
+        if(conversation[index].person.sprite.length) people[peopleIndex].sprite = conversation[index].person.sprite
+      }
+
+      people[peopleIndex].flipX = conversation[index].person.flipX
+      people[peopleIndex].flipY = conversation[index].person.flipY
+
+      // if (conversation[index].person.vfx){}
+
+      if(conversation[index].person){
+        setName(conversation[index].person.name)
+      }
+
+      // Dispalay dialouge
+      loop(speed, () => {
+        setDialogue((prevDialogue) => {
+          return conversation[index].dialogue.slice(0, prevDialogue.length + 1)
+        })
+      }, 
+      // max loop
+      conversation[index].dialogue.length)
+    }
+  }
+
+  const handleContinue = () => {
+    console.log('clicked')
+    const next = index + 1
+    if(conversation[next]){
+      setDialogue('')
+      displayDialogue(next)
+      setIndex(next)
+    }
+  }
+
   useEffect(() => {
     window.addEventListener('resize', scaleUI)
     // Fire the function on the first time
     scaleUI()
 
-    wait(0.5, () => setIsVisible(true))
+    wait(0.5, () => {
+      setIsVisible(true)
+      displayDialogue(index)
+    })
 
     // Cleanup: Remove event listener on component unmount
     return () => {
       window.removeEventListener('resize', scaleUI)
     }
-  }
-  , [])
+  }, [])
 
   return (
     <>
@@ -67,12 +160,16 @@ function App() {
       {
         // Add your UI here
       }
-      <div className='dialogue_wrapper' style={{ visibility: isVisible ? 'visible' : 'hidden' }}>
-        <div className='dialogue border bg-black'>
-          <p>Hi, I'm a dialogue box!</p>
+      <div 
+        className='dialogue_wrapper' 
+        style={{ visibility: isVisible ? 'visible' : 'hidden' }}
+        onClick={handleContinue}  
+      >
+        <div className='dialogue border bg-black disable-scrollbars'>
+          <p>{ dialogue }</p>
         </div>
-        <div className='name_tag border bg-black'>
-          name goes here
+        <div className='name_tag border bg-black' style={{ visibility: name ? 'visible' : 'hidden' }}>
+          { name }
         </div>
       </div>
     </div>
