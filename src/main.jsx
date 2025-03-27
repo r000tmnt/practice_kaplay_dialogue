5119714953e1775faf79d7bfb6c155a3d47d3f67
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-03-27 14:36:59 +0800

    [Update] Refactor. Export the kaplay instance to use it across components.

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import kaplay from 'kaplay'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
+  <StrictMode>
     <App />
-  // </StrictMode>,
+  </StrictMode>,
 )
-
-// Initialize the game
-kaplay({
-  width: 720,
-  height: 1280,
-  letterbox: true,
-  root: document.getElementById('game'),
-})
 
