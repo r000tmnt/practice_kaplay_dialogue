121a750b41f1ce93b2c8638f204c029c02737288
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-03-31 18:23:44 +0800

    [Update] Provide the redux store to the App

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import dialogueStore from './store/dialogue.js'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={dialogueStore}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
 
